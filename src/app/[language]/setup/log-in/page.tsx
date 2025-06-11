18e03c8e2656c47ec704a0468afc64d491389267
DANSRM02 <danielmercenaries417@gmail.com>
2025-06-11 12:46:09 -0500

    feat(Styling View) Log-in

--- src/app/[language]/setup/log-in/page.tsx
+++ src/app/[language]/setup/log-in/page.tsx
@@ -27,7 +27,7 @@ function LogIn() {
         },
         email: {
           placeholder: t("ComedianLoginForm.fields.emailPlaceholder"),
-          label: t("ComedianLoginForm .fields.emailLabel"),
+          label: t("ComedianLoginForm.fields.emailLabel"),
         },
       },
       actions: {
@@ -40,7 +40,7 @@ function LogIn() {
   return (
     <>
       <section
-        // className={classes["log-in"]}
+        className="flex justify-center items-center flex-wrap p-12 min-h-[35rem] gap-8 border-2 border-gray-300 rounded-lg"
         aria-label="Log in section"
         tabIndex={0}
       >
@@ -49,27 +49,31 @@ function LogIn() {
           paragraph={translations.intro.paragraph}
         />
         <form
-        //   className={classes["log-in__form"]}
+          className="flex flex-col gap-6 w-full max-w-[550px]"
           aria-label="Comedian log in form"
         >
-          <DefaultField
-            id="comedian-name"
-            placeholder={translations.fields.name.placeholder}
-            label={translations.fields.name.label}
-            color="primary"
-            type="text"
-            aria-label={translations.fields.name.label}
-          />
+          <fieldset className="border-2 border-gray-300 rounded-2xl p-8 mb-8 bg-gray-100 shadow-md relative min-w-0">
+            <legend className="text-lg font-bold default-text-color px-4 mb-4 bg-white rounded-lg shadow-sm relative">
+              {translations.intro.heading}
+            </legend>
+            <DefaultField
+              id="comedian-name"
+              placeholder={translations.fields.name.placeholder}
+              label={translations.fields.name.label}
+              color="primary"
+              type="text"
+              aria-label={translations.fields.name.label}
+            />
 
-          <DefaultField
-            id="comedian-email"
-            placeholder={translations.fields.email.placeholder}
-            label={translations.fields.email.label}
-            color="primary"
-            type="email"
-            aria-label={translations.fields.email.label}
-          />
-
+            <DefaultField
+              id="comedian-email"
+              placeholder={translations.fields.email.placeholder}
+              label={translations.fields.email.label}
+              color="primary"
+              type="email"
+              aria-label={translations.fields.email.label}
+            />
+          </fieldset>
           <Button
             type="button"
             variant={"primary"}
